b3332c40eac8cd778102025f488516d60626f092
Rob Court <robbie.court@gmail.com>
2017-04-12 21:00:52 +0100

    basic config

---
+++ html/conf/vfb.js
@@ -0,0 +1,10 @@
+G.setIdleTimeOut(-1);
+G.setOnSelectionOptions({unselected_transparent:false});
+
+
+    
+        
+            Model.getDatasources()[0].fetchVariable("VFB_00017894");
+            Instances.getInstance("VFB_00017894.VFB_00017894_meta");
+            resolve3D("VFB_00017894");
+        

c491be8f82c010a381be554cca426f0729669e26
Rob Court <robbie.court@gmail.com>
2017-04-15 12:53:53 +0100

    Update vfb.js

--- html/conf/vfb.js
+++ html/conf/vfb.js
@@ -1,10 +1,7 @@
 G.setIdleTimeOut(-1);
 G.setOnSelectionOptions({unselected_transparent:false});
 
-
+closole.log(parent.window.location.search);
     
-        
-            Model.getDatasources()[0].fetchVariable("VFB_00017894");
-            Instances.getInstance("VFB_00017894.VFB_00017894_meta");
-            resolve3D("VFB_00017894");
-        
+window.addVfbInd("VFB_00017894");        
+ 

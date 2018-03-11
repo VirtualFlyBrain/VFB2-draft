757d3871e04b357e7c2a3e16fd0cadbb1a8db3ae
Rob Court <robbie.court@gmail.com>
2018-03-11 16:15:39 +0000

    Hiding Footer to bypass issue
    Ref https://github.com/VirtualFlyBrain/geppetto-vfb/issues/143

--- html/conf/vfb.js
+++ html/conf/vfb.js
@@ -4,3 +4,6 @@ G.setOnSelectionOptions({unselected_transparent:false});
 try{var selection = parent.window.location.search;if (selection.indexOf("i=VFB") > -1) {var list = selection.substring(selection.indexOf("i=") + 2).split(",");if (list.length > 0) {window.addVfbIds(list);} else {window.addVfbIds(["VFB_00017894"]);}} else {window.addVfbIds(["VFB_00017894"]);}if (selection.indexOf("id=") > -1) {var info = "";info = selection.indexOf("&") > selection.indexOf("id=") ? selection.substring(selection.indexOf("id=") + 3, selection.indexOf("&")) : selection.substring(selection.indexOf("id=") + 3);if (info.length > 0) {window.addVfbIds([info]);}}} catch (err) {window.addVfbIds(["VFB_00017894"]);console.log(err.message);};
 
 Canvas1.engine.controls.rotateSpeed = 3;
+
+# Hiding entire footer until fixed:
+$('#footer').hide()
